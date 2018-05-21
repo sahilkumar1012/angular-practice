215240df9352bced35bceabb369fcf641908156b
sahil <sahilkumar1012@gmail.com>
2018-05-22 00:26:54 +0530

    base authentication is added using firebase, sending token to server

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -3,19 +3,24 @@ import { Http, Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { map } from 'rxjs/operators';
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
     constructor(private http: Http,
-        private recipeService: RecipeService) { }
+        private recipeService: RecipeService,
+        private authService : AuthService) { }
 
     storeRecipes() {
-        return  this.http.put('https://ng-recipe-book-sk.firebaseio.com/recipes.json',
+        const token = this.authService.getToken();
+        return  this.http.put('https://ng-recipe-book-sk.firebaseio.com/recipes.json?auth='+token,
             this.recipeService.getRecipes());
     }
 
     getRecipes(){
-        this.http.get('https://ng-recipe-book-sk.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        console.log('token is : '+ token);
+        this.http.get('https://ng-recipe-book-sk.firebaseio.com/recipes.json?auth='+token)
             .pipe(
                 map(
                     (response:Response)=>{
