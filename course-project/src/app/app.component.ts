bf61d42ee6b9febe2d2ee57bfa98965ed01162ab
sahil <sahilkumar1012@gmail.com>
2018-05-21 23:16:41 +0530

    components for signup and signin added

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   loadedFeature:string ='recipe';
 
   onNavigate(loadedFeature:any){
     this.loadedFeature=loadedFeature;
+  }
+
+  ngOnInit(){
+
   }
 
 }
