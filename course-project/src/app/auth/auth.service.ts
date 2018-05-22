231680feb8bf84fcd13e56c438c8919ab0371afe
sahil <sahilkumar1012@gmail.com>
2018-05-22 23:52:09 +0530

    authentication done in course-project

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService{
     token :string;
+
+    constructor(private router:Router){}
+
     signupUser(email:string,password:string){
         firebase.auth().createUserWithEmailAndPassword(email,password)
         .catch(
@@ -13,6 +19,7 @@ export class AuthService{
         firebase.auth().signInWithEmailAndPassword(email,password)
             .then(
                 response=>{
+                    this.router.navigate(['/']);
                     firebase.auth().currentUser.getIdToken()
                         .then(
                             (token : string)=>{this.token = token;}
@@ -24,11 +31,21 @@ export class AuthService{
             );
     }
 
+    signoutUser(){
+        firebase.auth().signOut();
+        this.router.navigate(['/signin']);
+        this.token=null;
+    }
+
     getToken(){
         firebase.auth().currentUser.getIdToken()
         .then(
             (token:string)=> this.token = token
         );
         return this.token;
+    }
+
+    isAuthenticated(){
+        return this.token!=null;
     }
 }
